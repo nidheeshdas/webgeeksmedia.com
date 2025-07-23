66a66b229971a419bf83e5d6b3ee75ebdbfb033e
nidheeshdas <nidheeshdas@gmail.com>
2025-07-23 15:48:22 +0530

    fixes features

--- app/components/FeaturesSection/FeaturesSection.tsx
+++ app/components/FeaturesSection/FeaturesSection.tsx
@@ -1,62 +1,294 @@
 import React from 'react';
-import ServiceCard from '../Card/Card';
-interface Service {
-  icon: string;
-  title: string;
-  description: string;
-  upcoming?: boolean;
-}
 
-const services: Service[] = [
-  {
-    icon: 'seo-audit',
-    title: 'SEO Audit Services',
-    description: 'Get expert eyes on your website. Start with our $49 entry-level audit and discover what’s holding back your rankings.',
-  },
-  {
-    icon: 'wordpress-care',
-    title: 'WordPress Care Plans',
-    description: 'From essential updates to elite care for eCommerce, we keep your site secure, fast, and worry-free — starting at just $49/month.',
-    upcoming: true,
-  },
-  {
-    icon: 'content-writing',
-    title: 'SEO Content Writing',
-    description: 'Scale your content the right way. Choose between high-quality human-written authority content or affordable AI-assisted bulk publishing.',
-    upcoming: true,
-  },
-  {
-    icon: 'link-building',
-    title: 'Link Building Services',
-    description: 'Gain high-authority, niche-relevant backlinks to grow your visibility, trust, and traffic. Tailored campaigns for startups and tough niches like CBD or Forex.',
-    upcoming: true,
-  },
-];
+const imgSvg = "/icons/seo-audit.svg";
+const imgSvg1 = "/icons/wordpress-care.svg";
+const imgSvg2 = "/icons/content-writing.svg";
+const imgSvg3 = "/icons/link-building.svg";
 
-const Services: React.FC = () => {
+const FeaturesSection: React.FC = () => {
   return (
-    <section id="services" className="bg-[#f8f8f8] box-border content-stretch flex flex-col gap-[40px] sm:gap-[50px] lg:gap-[60px] items-center justify-start px-5 sm:px-[60px] lg:px-[120px] py-16 sm:py-18 lg:py-20 relative size-full">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="box-border content-stretch flex flex-col gap-[12px] sm:gap-[15px] lg:gap-[18px] items-center justify-start leading-[0] not-italic p-0 relative shrink-0 text-center">
-          <h2 className="flex flex-col font-['Satoshi-Medium',_sans-serif] justify-center relative shrink-0 text-[#000000] text-[28px] sm:text-[34px] lg:text-[40px] w-full leading-tight">Our Core Services</h2>
-          <p className="flex flex-col font-['Satoshi-Regular',_sans-serif] justify-center relative shrink-0 text-[#333333] text-[16px] sm:text-[18px] lg:text-[20px] w-full max-w-3xl">
-            Comprehensive solutions tailored to boost your online presence and drive sustainable growth.
+    <div
+      className="bg-[#f8f8f8] box-border content-stretch flex flex-col gap-10 md:gap-16 items-center justify-start px-4 sm:px-8 md:px-16 lg:px-20 xl:px-[120px] py-10 md:py-20 relative size-full"
+      data-name="Background"
+      id="node-82_224"
+    >
+      <div
+        className="box-border content-stretch flex flex-col gap-[18px] items-center justify-start leading-[0] not-italic p-0 relative shrink-0 text-center w-full max-w-[858px]"
+        id="node-82_225"
+      >
+        <div
+          className="flex flex-col font-['Satoshi_Variable:Medium',_sans-serif] h-auto md:h-10 justify-center relative shrink-0 text-[#000000] text-3xl md:text-[40px] w-full"
+          id="node-82_226"
+        >
+          <p className="block leading-[40px]">Our Core Services</p>
+        </div>
+        <div
+          className="flex flex-col font-['Satoshi-Regular',_sans-serif] justify-center relative shrink-0 text-[#333333] text-base md:text-[20px] w-full"
+          id="node-82_227"
+        >
+          <p className="block leading-[30px]">
+            Comprehensive solutions tailored to boost your online presence and
+            drive sustainable growth.
           </p>
         </div>
-        <div className="mt-8 sm:mt-10 lg:mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-5">
-          {services.map((service) => (
-            <ServiceCard
-              key={service.title}
-              icon={service.icon}
-              title={service.title}
-              description={service.description}
-              upcoming={service.upcoming}
-            />
-          ))}
+      </div>
+      <div
+        className="grid w-full grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4"
+        id="node-82_228"
+      >
+        <div
+          className="bg-[#ffffff] h-[347px] relative rounded-[20px]"
+          data-name="Link"
+          id="node-82_229"
+        >
+          <div className="absolute border-[#d7c8ff] border-[0px_4px_4px_0px] border-solid inset-0 pointer-events-none rounded-[20px]" />
+          <div
+            className="absolute box-border content-stretch flex flex-col gap-4 items-start justify-start left-[25px] p-0 top-[30px] w-[238px]"
+            id="node-115_30"
+          >
+            <div
+              className="bg-[#4600ff] relative rounded-xl shrink-0 size-[45px]"
+              data-name="Search icon"
+              id="node-82_230"
+            >
+              <div
+                className="absolute left-2.5 size-[25px] top-2.5"
+                data-name="SVG"
+                id="node-82_231"
+              >
+                <div className="absolute bottom-[-0.01%] left-0 right-0 top-0">
+                  <img
+                    alt="SEO Audit"
+                    className="block max-w-none size-full"
+                    src={imgSvg}
+                  />
+                </div>
+              </div>
+            </div>
+            <div
+              className="bg-[#4600ff] bg-clip-text font-['Satoshi-Bold',_sans-serif] leading-[normal] min-w-full not-italic relative shrink-0 text-[#4600ff] text-[26px] text-left"
+              id="node-82_233"
+              style={{
+                WebkitTextFillColor: "transparent",
+                width: "min-content",
+              }}
+            >
+              <p className="block mb-0">{`SEO Audit `}</p>
+              <p className="block">Services</p>
+            </div>
+            <div
+              className="font-['Satoshi-Regular',_sans-serif] leading-[0] min-w-full not-italic relative shrink-0 text-[#333333] text-[17px] text-left"
+              id="node-82_234"
+              style={{ width: "min-content" }}
+            >
+              <p className="block leading-[normal]">
+                Get expert eyes on your website. Start with our $49 entry-level
+                audit and discover what’s holding back your rankings.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div
+          className="bg-[#ffffff] h-[347px] relative rounded-[20px]"
+          data-name="Link"
+          id="node-115_31"
+        >
+          <div className="absolute border-[#d7c8ff] border-[0px_4px_4px_0px] border-solid inset-0 pointer-events-none rounded-[20px]" />
+          <div
+            className="absolute box-border content-stretch flex flex-col gap-4 items-start justify-start left-[25px] p-0 top-[30px] w-[238px]"
+            id="node-115_32"
+          >
+            <div
+              className="box-border content-stretch flex flex-row items-start justify-between p-0 relative shrink-0 w-full"
+              id="node-139_23"
+            >
+              <div
+                className="relative rounded-xl shrink-0 size-[45px]"
+                data-name="WordPress icon"
+                id="node-115_38"
+              >
+                <div
+                  className="absolute left-2.5 size-[25px] top-2.5"
+                  data-name="SVG"
+                  id="node-115_39"
+                >
+                  <img
+                    alt="Wordpress Care"
+                    className="block max-w-none size-full"
+                    src={imgSvg1}
+                  />
+                </div>
+              </div>
+              <div
+                className="bg-[#ffffff] box-border content-stretch flex flex-row gap-2.5 items-center justify-center px-5 py-1.5 relative rounded-[31.858px] shrink-0"
+                id="node-139_35"
+              >
+                <div className="absolute border border-[#e67474] border-solid inset-[-0.5px] pointer-events-none rounded-[32.358px]" />
+                <div
+                  className="font-['Satoshi-Bold',_sans-serif] leading-[0] not-italic relative shrink-0 text-[#0037b1] text-[12px] text-center text-nowrap"
+                  id="node-139_36"
+                >
+                  <p className="block leading-[normal] whitespace-pre">
+                    Upcoming
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="bg-[#4600ff] bg-clip-text font-['Satoshi-Bold',_sans-serif] leading-[normal] not-italic relative shrink-0 text-[#4600ff] text-[26px] text-left w-full"
+              id="node-139_22"
+              style={{ WebkitTextFillColor: "transparent" }}
+            >
+              <p className="block mb-0">WordPress Care</p>
+              <p className="block">Plans</p>
+            </div>
+            <div
+              className="font-['Satoshi-Regular',_sans-serif] leading-[normal] not-italic relative shrink-0 text-[#333333] text-[17px] text-left w-full"
+              id="node-115_37"
+            >
+              <p className="block mb-0">From essential updates to elite</p>
+              <p className="block">
+                care for eCommerce, we keep your site secure, fast, and
+                worry-free — starting at just $49/month.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div
+          className="bg-[#ffffff] h-[347px] relative rounded-[20px]"
+          data-name="Link"
+          id="node-115_42"
+        >
+          <div className="absolute border-[#d7c8ff] border-[0px_4px_4px_0px] border-solid inset-0 pointer-events-none rounded-[20px]" />
+          <div
+            className="absolute box-border content-stretch flex flex-col gap-4 items-start justify-start left-[25px] p-0 top-[30px] w-[238px]"
+            id="node-115_43"
+          >
+            <div
+              className="box-border content-stretch flex flex-row items-start justify-between p-0 relative shrink-0 w-full"
+              id="node-139_30"
+            >
+              <div
+                className="relative rounded-xl shrink-0 size-[45px]"
+                data-name="Pen Icon"
+                id="node-115_49"
+              >
+                <div
+                  className="absolute left-2.5 size-[25px] top-2.5"
+                  data-name="SVG"
+                  id="node-115_50"
+                >
+                  <img
+                    alt="Content Writing"
+                    className="block max-w-none size-full"
+                    src={imgSvg2}
+                  />
+                </div>
+              </div>
+              <div
+                className="bg-[#ffffff] box-border content-stretch flex flex-row gap-2.5 items-center justify-center px-5 py-1.5 relative rounded-[31.858px] shrink-0"
+                id="node-139_27"
+              >
+                <div className="absolute border border-[#e67474] border-solid inset-[-0.5px] pointer-events-none rounded-[32.358px]" />
+                <div
+                  className="font-['Satoshi-Bold',_sans-serif] leading-[0] not-italic relative shrink-0 text-[#0037b1] text-[12px] text-center text-nowrap"
+                  id="node-139_28"
+                >
+                  <p className="block leading-[normal] whitespace-pre">
+                    Upcoming
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="bg-[#4600ff] bg-clip-text font-['Satoshi-Bold',_sans-serif] leading-[0] not-italic relative shrink-0 text-[#4600ff] text-[26px] text-left w-full"
+              id="node-115_47"
+              style={{ WebkitTextFillColor: "transparent" }}
+            >
+              <p className="block leading-[normal]">SEO Content Writing</p>
+            </div>
+            <div
+              className="font-['Satoshi-Regular',_sans-serif] leading-[0] not-italic relative shrink-0 text-[#333333] text-[17px] text-left w-full"
+              id="node-115_48"
+            >
+              <p className="block leading-[normal]">
+                Scale your content the right way. Choose between high-quality
+                human-written authority content or affordable AI-assisted bulk
+                publishing.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div
+          className="bg-[#ffffff] h-[347px] relative rounded-[20px]"
+          data-name="Link"
+          id="node-115_53"
+        >
+          <div className="absolute border-[#d7c8ff] border-[0px_4px_4px_0px] border-solid inset-0 pointer-events-none rounded-[20px]" />
+          <div
+            className="absolute box-border content-stretch flex flex-col gap-4 items-start justify-start left-[25px] p-0 top-[30px] w-[238px]"
+            id="node-115_54"
+          >
+            <div
+              className="box-border content-stretch flex flex-row items-start justify-between p-0 relative shrink-0 w-full"
+              id="node-139_34"
+            >
+              <div
+                className="relative rounded-xl shrink-0 size-[45px]"
+                data-name="Link icon"
+                id="node-115_60"
+              >
+                <div
+                  className="absolute left-2.5 size-[25px] top-2.5"
+                  data-name="SVG"
+                  id="node-115_61"
+                >
+                  <img
+                    alt="Link Building"
+                    className="block max-w-none size-full"
+                    src={imgSvg3}
+                  />
+                </div>
+              </div>
+              <div
+                className="bg-[#ffffff] box-border content-stretch flex flex-row gap-2.5 items-center justify-center px-5 py-1.5 relative rounded-[31.858px] shrink-0"
+                id="node-139_38"
+              >
+                <div className="absolute border border-[#e67474] border-solid inset-[-0.5px] pointer-events-none rounded-[32.358px]" />
+                <div
+                  className="font-['Satoshi-Bold',_sans-serif] leading-[0] not-italic relative shrink-0 text-[#0037b1] text-[12px] text-center text-nowrap"
+                  id="node-139_39"
+                >
+                  <p className="block leading-[normal] whitespace-pre">
+                    Upcoming
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="bg-[#4600ff] bg-clip-text font-['Satoshi-Bold',_sans-serif] leading-[normal] not-italic relative shrink-0 text-[#4600ff] text-[26px] text-left w-full"
+              id="node-115_58"
+              style={{ WebkitTextFillColor: "transparent" }}
+            >
+              <p className="block mb-0">Link Building</p>
+              <p className="block">Services</p>
+            </div>
+            <div
+              className="font-['Satoshi-Regular',_sans-serif] leading-[normal] not-italic relative shrink-0 text-[#333333] text-[17px] text-left w-full"
+              id="node-115_59"
+            >
+              <p className="block mb-0">Gain high-authority, niche-</p>
+              <p className="block mb-0">relevant backlinks to grow your</p>
+              <p className="block">
+                visibility, trust, and traffic. Tailored campaigns for startups
+                and tough niches like CBD or Forex.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
-export default Services;
+export default FeaturesSection;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <HeroSection />
         <Services />
         <TestimonialSection />
-        <WhyUs />
+        {/* <WhyUs /> */}
         <Cta />
       </main>
       <Footer />
