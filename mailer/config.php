6e82fc82608a469b9773c2433ef1af64dbec2f7d
nidheeshdas <nidheeshdas@gmail.com>
2025-08-05 09:32:09 +0530

    Update config.php

--- mailer/config.php
+++ mailer/config.php
@@ -8,8 +8,12 @@ use PayPalCheckoutSdk\Core\ProductionEnvironment as Environment; // For producti
 
 class PayPalClient {
     public static function client(): PayPalHttpClient {
-        $clientId     = 'ATBeztKZNDgAz9JdBoJkTRc3HfU3t_7iwIeDvs8S1lkPtM0wiN2lRp_JqRgiWhrXSYjTWoqmrVteeZWD';
-        $clientSecret = 'EDm4pzrG-9SKy_Wq0dt18KFg5HczJlu50Hx0KIqfwKRsHZMBAAkfVzWkSn_vI6l9UKmRgnwAy5ZeA13i';
+        // $clientId     = 'ATBeztKZNDgAz9JdBoJkTRc3HfU3t_7iwIeDvs8S1lkPtM0wiN2lRp_JqRgiWhrXSYjTWoqmrVteeZWD';
+        // $clientSecret = 'EDm4pzrG-9SKy_Wq0dt18KFg5HczJlu50Hx0KIqfwKRsHZMBAAkfVzWkSn_vI6l9UKmRgnwAy5ZeA13i';
+
+        $clientId = 'AVjtdOcb_yNFUTx_4iKoX4sJ-aSfRbaEKr9OZQxYKZwiTRT50E3cm-WrbotplxKpaR7Kef9wc784pEya';
+        $clientSecret = 'EM3gFfvUvU-KH-Rk4QabNQC7UN0I7y_wlvMur52dBXO4Guo5f13TRMpCergZBORgUpEVaerfG5r41qDH';
+
         $environment  = new Environment($clientId, $clientSecret);
         return new PayPalHttpClient($environment);
     }
